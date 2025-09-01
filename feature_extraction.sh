77204ace7c59c530b6c5bea093989d8206c8e75a
Israfel Salazar <israfel.sr@gmail.com>
2025-09-01 12:12:52 +0200

    feature extractor paligemma

---
+++ feature_extraction.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --account EUHPC_D27_102
+#SBATCH --job-name=extract_features
+#SBATCH --partition=boost_usr_prod
+#SBATCH --gres=gpu:1            # 1 GPU
+#SBATCH --cpus-per-task=8       # 1/4 of 32 CPUs
+#SBATCH --mem=120G              # ~1/4 of total RAM (≈480 GB / 4)
+#SBATCH --time=06:00:00         # walltime (hh:mm:ss)
+#SBATCH --output=%x_%j.out
+
+# Run your job
+export PYTHONPATH=$(pwd)
+python scripts/extract_features.py \
+    --config config/paligemma2-3b-ft-docci-448.yaml
